431cfc31eb0d0c888f439e13920de68ec5a6abc2
Philip Snyder <psnyder385@gmail.com>
2022-06-05 12:53:45 -0400

    Initial Commit

---
+++ contentScript.js
@@ -0,0 +1,11 @@
+
+function checkWindow() {
+    if(window.location.pathname == '/' || window.location.pathname == '/feed/explore') { // check for redir to subscriptions option
+        window.location.replace('https://www.youtube.com/feed/subscriptions')
+    } else if(window.location.pathname.includes('/shorts/') && window.location.pathname.length == 19) { // check for No-Shorts Option
+        // console.log('shorts:', window.location.pathname)
+        window.location.replace('https://www.youtube.com/feed/subscriptions')
+    }
+}
+
+setInterval(checkWindow, 500)
