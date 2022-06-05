fd88db6349b6af689f4f805574240f793f14cf81
Philip Snyder <psnyder385@gmail.com>
2022-06-05 17:29:59 -0400

    Updated Polling Logic

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,15 @@
 
+var lastHref = ''
+
 function checkWindow() {
-    if(window.location.pathname == '/' || window.location.pathname == '/feed/explore') { // check for redir to subscriptions option
-        window.location.replace('https://www.youtube.com/feed/subscriptions')
-    } else if(window.location.pathname.includes('/shorts/') && window.location.pathname.length == 19) { // check for No-Shorts Option
-        // console.log('shorts:', window.location.pathname)
-        window.location.replace('https://www.youtube.com/feed/subscriptions')
+    if (window.location.href != lastHref) {
+        lastHref = window.location.href
+        if(window.location.pathname == '/' || window.location.pathname == '/feed/explore') { // check for redir to subscriptions option
+            window.location.replace('https://www.youtube.com/feed/subscriptions')
+        } else if(window.location.pathname.includes('/shorts/') && window.location.pathname.length == 19) { // check for No-Shorts Option
+            // console.log('shorts:', window.location.pathname)
+            window.location.replace('https://www.youtube.com/feed/subscriptions')
+        }
     }
 }
 
