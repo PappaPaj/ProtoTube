cfafe1b294241ab7d384a7de70651ec0c08de969
Philip Snyder <psnyder385@gmail.com>
2022-06-11 21:46:41 -0400

    Added settings communication channels

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,26 @@
 
 var lastHref = ''
+var subOnly = true
+var antiShorts = true
+
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if(msg.subject ===  'settings') {
+        subOnly = msg.subOnly
+        antiShorts = msg.noShorts
+        console.log("Updated Settings")
+    }
+});
+
+
+
+
 
 function checkWindow() {
     if (window.location.href != lastHref) {
         lastHref = window.location.href
-        if(window.location.pathname == '/' || window.location.pathname == '/feed/explore') { // check for redir to subscriptions option
+        if((window.location.pathname == '/' || window.location.pathname == '/feed/explore') && subOnly) { // check for redir to subscriptions option
             window.location.replace('https://www.youtube.com/feed/subscriptions')
-        } else if(window.location.pathname.includes('/shorts/') && window.location.pathname.length == 19) { // check for No-Shorts Option
-            // console.log('shorts:', window.location.pathname)
+        } else if(window.location.pathname.includes('/shorts/') && window.location.pathname.length == 19 && antiShorts) { // check for No-Shorts Option
             window.location.replace('https://www.youtube.com/feed/subscriptions')
         }
     }
