cd557926dc5cb285893bf0a231e8c7175bc3a00d
Philip Snyder <psnyder385@gmail.com>
2022-07-10 11:25:13 -0400

    Hide Reels from Subscriptions Feed

--- contentScript.js
+++ contentScript.js
@@ -1,29 +1,46 @@
-
-var lastHref = ''
-var subOnly = true
-var antiShorts = true
+var lastHref = "";
+var lastLenReels = 0;
+var subOnly = true;
+var antiShorts = true;
+var hideShorts = true;
 
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-    if(msg.subject ===  'settings') {
-        subOnly = msg.subOnly
-        antiShorts = msg.noShorts
-        console.log("Updated Settings")
-    }
+  if (msg.subject === "settings") {
+    subOnly = msg.subOnly;
+    antiShorts = msg.noShorts;
+    hideShorts = msg.hideShorts;
+    console.log("Updated Settings");
+  }
 });
 
-
-
-
-
 function checkWindow() {
-    if (window.location.href != lastHref) {
-        lastHref = window.location.href
-        if((window.location.pathname == '/' || window.location.pathname == '/feed/explore') && subOnly) { // check for redir to subscriptions option
-            window.location.replace('https://www.youtube.com/feed/subscriptions')
-        } else if(window.location.pathname.includes('/shorts/') && window.location.pathname.length == 19 && antiShorts) { // check for No-Shorts Option
-            window.location.replace('https://www.youtube.com/feed/subscriptions')
-        }
+  if (window.location.pathname == "/feed/subscriptions" && hideShorts) {
+    let eles = document.getElementsByTagName("ytd-grid-video-renderer");
+    for (const element of eles) {
+      if (element.querySelector('[overlay-style="SHORTS"]') != undefined) {
+        // console.log(element);
+        element.parentNode.removeChild(element);
+      }
     }
+  }
+  if (window.location.href != lastHref) {
+    lastHref = window.location.href;
+    if (
+      (window.location.pathname == "/" ||
+        window.location.pathname == "/feed/explore") &&
+      subOnly
+    ) {
+      // check for redir to subscriptions option
+      window.location.replace("https://www.youtube.com/feed/subscriptions");
+    } else if (
+      window.location.pathname.includes("/shorts/") &&
+      window.location.pathname.length == 19 &&
+      antiShorts
+    ) {
+      // check for No-Shorts Option
+      window.location.replace("https://www.youtube.com/feed/subscriptions");
+    }
+  }
 }
 
-setInterval(checkWindow, 500)
+setInterval(checkWindow, 500);
--- popup.js
+++ popup.js
@@ -1,76 +1,91 @@
-
 function getAllStorageSyncData() {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.get(null, (items) => {
-        if (chrome.runtime.lastError) {
-            return reject(chrome.runtime.lastError);
-        }
-        resolve(items);
-        });
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(null, (items) => {
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError);
+      }
+      resolve(items);
     });
+  });
 }
 // getAllStorageSyncData().then(res => console.log(res))
 
-
-const subOnly = document.getElementById('subonly-toggle')
-const noShorts = document.getElementById('noshorts-toggle')
-
-
-
+const subOnly = document.getElementById("subonly-toggle");
+const noShorts = document.getElementById("noshorts-toggle");
+const hideShorts = document.getElementById("hideshorts-toggle");
 
 // Init
-chrome.storage.sync.get('subonly-toggle', (val) => {
-    if (val['subonly-toggle'] != undefined) {
-        subOnly.checked = val['subonly-toggle']
-    }
-})
+chrome.storage.sync.get("subonly-toggle", (val) => {
+  if (val["subonly-toggle"] != undefined) {
+    subOnly.checked = val["subonly-toggle"];
+  }
+});
 
-chrome.storage.sync.get('noshorts-toggle', (val) => {
-    if (val['noshorts-toggle'] != undefined) {
-        noShorts.checked = val['noshorts-toggle']
-    }
-})
+chrome.storage.sync.get("noshorts-toggle", (val) => {
+  if (val["noshorts-toggle"] != undefined) {
+    noShorts.checked = val["noshorts-toggle"];
+  }
+});
 
+chrome.storage.sync.get("hideshorts-toggle", (val) => {
+  if (val["hideshorts-toggle"] != undefined) {
+    hideShorts.checked = val["hideshorts-toggle"];
+  }
+});
 
+window.addEventListener("DOMContentLoaded", () => {
+  // ...query for the active tab...
 
-window.addEventListener('DOMContentLoaded', () => {
-    // ...query for the active tab...
-
-    sendUpdatedSettings()
-      
+  sendUpdatedSettings();
 });
 
 function sendUpdatedSettings() {
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-      }, tabs => {
-        // ...and send a request for the DOM info...
-        let currTab = new URL(tabs[0].url)
-        if(currTab.host !== 'www.youtube.com') {
-            return
-        }
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                {from: 'popup', subject: 'settings', subOnly: subOnly.checked, noShorts: noShorts.checked}).catch((error) => {
-                    console.log("Content Script not Found")
-                })  
-      });
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      // ...and send a request for the DOM info...
+      let currTab = new URL(tabs[0].url);
+      if (currTab.host !== "www.youtube.com") {
+        return;
+      }
+      chrome.tabs
+        .sendMessage(tabs[0].id, {
+          from: "popup",
+          subject: "settings",
+          subOnly: subOnly.checked,
+          noShorts: noShorts.checked,
+          hideShorts: hideShorts.checked,
+        })
+        .catch((error) => {
+          console.log("Content Script not Found");
+        });
+    }
+  );
 }
 
+subOnly.addEventListener("input", function (e) {
+  var setting = e.target.checked;
+  chrome.storage.sync.set({ "subonly-toggle": setting }, function () {
+    console.log("Value is set to " + setting);
+  });
+  sendUpdatedSettings();
+});
 
-subOnly.addEventListener('input', function(e) {
-    var setting = e.target.checked 
-    chrome.storage.sync.set({'subonly-toggle': setting}, function() {
-        console.log('Value is set to ' + setting);
-    });
-    sendUpdatedSettings();
-})
+noShorts.addEventListener("input", function (e) {
+  var setting = e.target.checked;
+  chrome.storage.sync.set({ "noshorts-toggle": setting }, function () {
+    console.log("Value is set to " + setting);
+  });
+  sendUpdatedSettings();
+});
 
-noShorts.addEventListener('input', function(e) {
-    var setting = e.target.checked 
-    chrome.storage.sync.set({'noshorts-toggle': setting}, function() {
-        console.log('Value is set to ' + setting);
-    });
-    sendUpdatedSettings();
-})
+hideShorts.addEventListener("input", function (e) {
+  var setting = e.target.checked;
+  chrome.storage.sync.set({ "hideshorts-toggle": setting }, function () {
+    console.log("Value is set to " + setting);
+  });
+  sendUpdatedSettings();
+});
